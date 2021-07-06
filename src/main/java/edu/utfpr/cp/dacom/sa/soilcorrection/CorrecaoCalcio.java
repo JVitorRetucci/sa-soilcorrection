147164139c0869a7c85e4e0bd168c12194947d78
João Vítor Retucci <jvitor.200003@gmail.com>
2021-07-05 21:55:36 -0300

    Testa participacao ideal do calcio

--- src/main/java/edu/utfpr/cp/dacom/sa/soilcorrection/CorrecaoCalcio.java
+++ src/main/java/edu/utfpr/cp/dacom/sa/soilcorrection/CorrecaoCalcio.java
@@ -1,10 +1,26 @@
 package edu.utfpr.cp.dacom.sa.soilcorrection;
 
 public class CorrecaoCalcio {
-  public double calculaParticipacaoCalcioCtcSolo(double calcio, double magnesio, double potassio,
+  public double calculaParticipacaoCalcioCtcSolo(
+      double calcio, 
+      double magnesio, 
+      double potassio,
       double aluminioHidrogenio) {
 
     return calcio / new EquilibrioCorrecaoCTC().calculaCTCCmol(potassio, calcio, magnesio, aluminioHidrogenio);
 
   }
+
+  public String participacaoIdealCalcioCtcSolo(int textura) {
+    String participacaoIdeal;
+    
+    switch (textura) {
+      case 0 -> participacaoIdeal = "45 a 55"; //Solo Argiloso
+      case 1 -> participacaoIdeal = "35 a 40"; //Solo Textua Média
+      default -> throw new IllegalArgumentException();
+    }
+
+    return participacaoIdeal;
+  }
+
 }
--- src/test/java/edu/utfpr/cp/dacom/sa/soilcorrection/TestaCorrecaoCalcioMagnesio.java
+++ src/test/java/edu/utfpr/cp/dacom/sa/soilcorrection/TestaCorrecaoCalcioMagnesio.java
@@ -10,4 +10,10 @@ public class TestaCorrecaoCalcioMagnesio {
 
 		assertEquals(0.4468580294802172, new CorrecaoCalcio().calculaParticipacaoCalcioCtcSolo(5.76, 1.63, 0.15, 5.35));
 	}
+
+	@Test
+	public void testaParticipaoIdealCalcio() {
+		assertEquals("45 a 55", new CorrecaoCalcio().participacaoIdealCalcioCtcSolo(0)); //Teste argiloso
+		assertEquals("35 a 40", new CorrecaoCalcio().participacaoIdealCalcioCtcSolo(1)); //Teste textura média
+	}
 }
